68cce9e7068ab1ae3ebf492d2d23d8799a6f5353
tayan <tayan@TayanPc>
2023-06-25 17:04:14 +0300

    pushing the code

---
+++ Project_1/src/pkg1/Student.java
@@ -0,0 +1,15 @@
+package pkg1;
+
+public class Student {
+	int a;
+	public void tayan()
+	{
+		System.out.println("Welcome to all of you");
+	}
+	public static void main(String[] args)
+	{
+		Student abc=new Student();
+		abc.tayan();
+	}
+			
+}
